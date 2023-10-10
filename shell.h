8afa7d2795f7f592b9c82f9a2034c989c5d8adcc
abel-alx-github <abeltibeso323@gmail.com>
2023-10-10 05:52:17 -0700

    simple_shell main file and other

--- shell.h
+++ shell.h
@@ -5,9 +5,11 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 void shell_print(const char *text);
 void prompt_message(void);
 void team_memebers(void);
+void read_command(char cmd[], char *para[]);
 int intepreter(void);/* why did you add this?*/
 void read_stdin(char* command);
 #endif
