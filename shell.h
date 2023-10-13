157eba85d4ed80d5b0a0af30d8ffb28f7aa8dd78
oletilwe <oletilwelephalala8@gmail.com>
2023-10-13 04:46:42 -0700

    0x16. C - Simple Shell

--- shell.h
+++ shell.h
@@ -13,4 +13,6 @@ int interpreter(void);
 void read_command(char cmd[], char *para[]);
 int intepreter(void);
 void read_stdin(char *command);
+void read_input(char *input); /* this one as well */
+void execute_command(char *input); /* from the code i added */
 #endif
