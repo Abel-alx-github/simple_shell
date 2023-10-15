a37efaa722f8a3eac2eb0cfc19a1c587b10e21b0
oletilwe <oletilwelephalala8@gmail.com>
2023-10-15 06:12:53 -0700

    command problems

--- stay_here/Oletilwe_practice_here/command.c
+++ stay_here/Oletilwe_practice_here/command.c
@@ -17,6 +17,7 @@
 void read_input(char *input)
 	{
 		ssize_t bytes_read;
+
 		write(STDOUT_FILENO, "> ", 2);
 		bytes_read = read(STDIN_FILENO, input, MAX_INPUT_SIZE);
 		if (bytes_read > 0 && input[bytes_read - 1] == '\n')
@@ -67,45 +68,49 @@ void execute_command(char *input)
 /**
 * execute_command - to handle a path
 * @input: input value
+* Return: always 0
 */
 int execute_command(char *input)
 	{
-		pid_t pid
+		pid_t pid;
 		char *args[MAX_ARGS];
-    		char *token = strtok(input, " ");
-    		int i = 0;
+		char *token = strtok(input, " ");
+		int i = 0;
+
 		while (token != NULL)
-		{
-			args[i++] = token;
-			token = strtok(NULL, " ");
-		}
+	{
+		args[i++] = token;
+		token = strtok(NULL, " ");
+	}
 		args[i] = NULL;
 		pid = fork();
 		if (pid == -1)
-		{
-			perror("fork");
-		} else if (pid == 0)
-		{
-			char *envp[] = { NULL };
-			if (execve(args[0], args, envp ) == -1)
-			{
-				perror("execve");
-				exit(EXIT_FAILURE);
-			}
-			else
-			{
-				int status;
-				waitpid(pid, &status, 0);
-				if (WIFEXITED(status)) 
-				{
-					if (WEXITSTATUS(status) == 127)
-					{
-						char error_message[100];
-						sprintf(error_message, "command not found: %s\n", args[0];
-						write(STDOUT_FILENO, error_message, strlen(error_message));
-						return (-1);
-						}
-						}
-						}
-						return (0);
-						}
+	{
+		perror("fork");
+	} else if (pid == 0)
+	{
+		char *envp[] = { NULL };
+
+		if (execve(args[0], args, envp) == -1)
+	{
+		perror("execve");
+		exit(EXIT_FAILURE);
+	} else
+	{
+		int status;
+
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+	{
+		if (WEXITSTATUS(status) == 127)
+	{
+		char error_message[100];
+
+		sprintf(error_message, "command not found: %s\n", args[0]);
+		write(STDOUT_FILENO, error_message, strlen(error_message));
+		return (-1);
+	}
+	}
+	}
+	return (0);
+	}
