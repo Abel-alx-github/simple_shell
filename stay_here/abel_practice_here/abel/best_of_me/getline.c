183a7cc9d43ea9ed9013d53814db8de584e0ffbe
oletilwe <oletilwelephalala8@gmail.com>
2023-10-17 02:24:00 -0700

    file updated with betty style

--- stay_here/abel_practice_here/abel/best_of_me/getline.c
+++ stay_here/abel_practice_here/abel/best_of_me/getline.c
@@ -1,24 +1,34 @@
 #include "shell.h"
+
+/**
+* custom_getline - reads a line of an input stream
+* @lineptr: memory leak
+* @n: input value
+* @stream: input value
+* Return: always 0
+*/
 ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
 {
-  char *buffer = malloc(MAX_INPUT_SIZE * sizeof(char));
-  int position = 0;
-  int num_chars_read = 0;
+	char *buffer = malloc(MAX_INPUT_SIZE * sizeof(char));
+	int position = 0;
+	int num_chars_read = 0;
 
-  while (1) {
-    char current_char = fgetc(stream);
+	while (1)
+	{
+	char current_char = fgetc(stream);
 
-    if (current_char == EOF || current_char == '\n') {
-      break;
-    }
+	if (current_char == EOF || current_char == '\n')
+	{
+	break;
+	}
 
-    buffer[position++] = current_char;
-    num_chars_read++;
-  }
+	buffer[position++] = current_char;
+	num_chars_read++;
+	}
 
-  buffer[position] = '\0';
-  *lineptr = buffer;
-  *n = num_chars_read;
+	buffer[position] = '\0';
+	*lineptr = buffer;
+	*n = num_chars_read;
 
-  return num_chars_read;
+	return (num_chars_read);
 }
